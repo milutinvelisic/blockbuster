1891451e7e6f541df62d8c433e037545bc9cec3e
milutinvelisic <44348167+milutinvelisic@users.noreply.github.com>
2019-06-11 20:46:47 +0200

    Add files via upload

--- assets/admin/js/script.js
+++ assets/admin/js/script.js
@@ -1,9 +1,19 @@
 window.onload = function () {
 
     if ($("#datatype")) {
-        $("#datatype").click(function () {
+        $("#datatype").change(function () {
             let forma = $(this).val();
 
+            if(forma == 0){
+                $("#formBlog").hide('fast');
+                $("#formGenre").hide('fast');
+                $("#formMovie").hide('fast');
+                $("#formSeries").hide('fast');
+                $("#formDirector").hide('fast');
+                $("#formProducer").hide('fast');
+                $("#formCat").hide('fast');
+                $("#formActor").hide('fast');
+            }
             if (forma == 1) {
                 $("#formBlog").hide('fast');
                 $("#formGenre").hide('fast');
@@ -100,4 +110,266 @@ window.onload = function () {
             }
         })
     }
+
+    if($("#showtype")){
+        $("#showtype").change(function(){
+            let forma = $(this).val()
+            console.log(forma)
+
+            if(forma == 0){
+                $("#dataBlog").hide('fast');
+                $("#dataGenre").hide('fast');
+                $("#dataMovie").hide('fast');
+                $("#dataSeries").hide('fast');
+                $("#dataDirector").hide('fast');
+                $("#dataProducer").hide('fast');
+                $("#dataCat").hide('fast');
+                $("#dataActor").hide('fast');
+            }
+            if (forma == 1) {
+                $("#dataBlog").hide('fast');
+                $("#dataGenre").hide('fast');
+                $("#dataMovie").hide('fast');
+                $("#dataSeries").hide('fast');
+                $("#dataDirector").hide('fast');
+                $("#dataProducer").hide('fast');
+                $("#dataCat").hide('fast');
+                $("#dataActor").fadeIn('slow');
+            }
+
+            if (forma == 2) {
+                $("#dataActor").hide('fast');
+                $("#dataBlog").hide('fast');
+                $("#dataMovie").hide('fast');
+                $("#dataSeries").hide('fast');
+                $("#dataCat").hide('fast');
+                $("#dataGenre").hide('fast');
+                $("#dataDirector").fadeIn('slow');
+                $("#dataProducer").fadeIn('slow');
+
+            }
+
+            if (forma == 3) {
+                $("#dataActor").hide('fast');
+                $("#dataGenre").hide('fast');
+                $("#dataMovie").hide('fast');
+                $("#dataSeries").hide('fast');
+                $("#dataDirector").hide('fast');
+                $("#dataProducer").hide('fast');
+                $("#dataCat").hide('fast');
+                $("#dataBlog").fadeIn('slow');
+
+            }
+
+            if (forma == 4) {
+                $("#dataBlog").hide('fast');
+                $("#dataActor").hide('fast');
+                $("#dataGenre").hide('fast');
+                $("#dataSeries").hide('fast');
+                $("#dataDirector").hide('fast');
+                $("#dataProducer").hide('fast');
+                $("#dataCat").hide('fast');
+                $("#dataMovie").fadeIn('slow');
+
+            }
+
+            if (forma == 5) {
+                $("#dataBlog").hide('fast');
+                $("#dataActor").hide('fast');
+                $("#dataGenre").hide('fast');
+                $("#dataMovie").hide('fast');
+                $("#dataDirector").hide('fast');
+                $("#dataProducer").hide('fast');
+                $("#dataCat").hide('fast');
+                $("#dataSeries").fadeIn('slow');
+
+            }
+
+            if (forma == 8) {
+                $("#dataBlog").hide('fast');
+                $("#dataActor").hide('fast');
+                $("#dataGenre").hide('fast');
+                $("#dataMovie").hide('fast');
+                $("#dataSeries").hide('fast');
+                $("#dataDirector").hide('fast');
+                $("#dataProducer").hide('fast');
+                $("#dataCat").fadeIn('slow');
+                $("#dataGenre").fadeIn('slow');
+
+            }
+        })
+    }
+
+    if($("#connecttype")){
+        $("#connecttype").change(function(){
+            let forma = $(this).val();
+
+            if(forma == 0){
+                $("#connectActorMS").hide("fast");
+                $("#connectMSActor").hide("fast");
+                $("#btnConnectActorMS").hide("fast");
+                $("#connectGenreMS").hide("fast");
+                $("#connectMSGenre").hide("fast");
+                $("#btnConnectMSGenre").hide("fast");
+                $("#connectCatMS").hide("fast");
+                $("#connectMSCat").hide("fast");
+                $("#btnConnectMSCat").hide("fast");
+                $("#connectDirectorMS").hide("fast");
+                $("#connectMSDirector").hide("fast");
+                $("#btnConnectMSDirector").hide("fast");
+                $("#connectProducerMS").hide("fast");
+                $("#connectMSProducer").hide("fast");
+                $("#btnConnectMSProducer").hide("fast");
+            }
+
+            if(forma == 1){
+                $("#connectGenreMS").hide("fast");
+                $("#connectMSGenre").hide("fast");
+                $("#btnConnectMSGenre").hide("fast");
+                $("#connectCatMS").hide("fast");
+                $("#connectMSCat").hide("fast");
+                $("#btnConnectMSCat").hide("fast");
+                $("#connectDirectorMS").hide("fast");
+                $("#connectMSDirector").hide("fast");
+                $("#btnConnectMSDirector").hide("fast");
+                $("#connectProducerMS").hide("fast");
+                $("#connectMSProducer").hide("fast");
+                $("#btnConnectMSProducer").hide("fast");
+                $("#connectActorMS").fadeIn("slow");
+                $("#connectMSActor").fadeIn("slow");
+                $("#btnConnectActorMS").fadeIn("slow");
+                $("#connectactorms").change(function(){
+                    let form1 = $(this).val()
+                    console.log(form1)
+                    $("#connectmsactor").change(function(){
+                        let form2 = $(this).val()
+                        console.log(form2)
+                        if((form1 && form2) != 0){
+                            $("#btnConnectActorMS button").removeAttr("disabled")
+                        }
+                    })
+                })
+            }
+
+            if(forma == 2){
+                $("#connectActorMS").hide("fast");
+                $("#connectMSActor").hide("fast");
+                $("#btnConnectActorMS").hide("fast");
+                $("#connectCatMS").hide("fast");
+                $("#connectMSCat").hide("fast");
+                $("#btnConnectMSCat").hide("fast");
+                $("#connectDirectorMS").hide("fast");
+                $("#connectMSDirector").hide("fast");
+                $("#btnConnectMSDirector").hide("fast");
+                $("#connectProducerMS").hide("fast");
+                $("#connectMSProducer").hide("fast");
+                $("#btnConnectMSProducer").hide("fast");
+                $("#connectGenreMS").fadeIn("slow");
+                $("#connectMSGenre").fadeIn("slow");
+                $("#btnConnectMSGenre").fadeIn("slow");
+
+                $("#connectgenrems").change(function(){
+                    let form1 = $(this).val()
+                    console.log(form1)
+                    $("#connectmsgenre").change(function(){
+                        let form2 = $(this).val()
+                        console.log(form2)
+                        if((form1 && form2) != 0){
+                            $("#btnConnectMSGenre button").removeAttr("disabled")
+                        }
+                    })
+                })
+            }
+
+            if(forma == 3){
+                $("#connectActorMS").hide("fast");
+                $("#connectMSActor").hide("fast");
+                $("#btnConnectActorMS").hide("fast");
+                $("#connectGenreMS").hide("fast");
+                $("#connectMSGenre").hide("fast");
+                $("#btnConnectMSGenre").hide("fast");
+                $("#connectDirectorMS").hide("fast");
+                $("#connectMSDirector").hide("fast");
+                $("#btnConnectMSDirector").hide("fast");
+                $("#connectProducerMS").hide("fast");
+                $("#connectMSProducer").hide("fast");
+                $("#btnConnectMSProducer").hide("fast");
+                $("#connectCatMS").fadeIn("slow");
+                $("#connectMSCat").fadeIn("slow");
+                $("#btnConnectMSCat").fadeIn("slow");
+
+                $("#connectcatms").change(function(){
+                    let form1 = $(this).val()
+                    console.log(form1)
+                    $("#connectmscat").change(function(){
+                        let form2 = $(this).val()
+                        console.log(form2)
+                        if((form1 && form2) != 0){
+                            $("#btnConnectMSCat button").removeAttr("disabled")
+                        }
+                    })
+                })
+            }
+
+            if(forma == 4){
+                $("#connectActorMS").hide("fast");
+                $("#connectMSActor").hide("fast");
+                $("#btnConnectActorMS").hide("fast");
+                $("#connectGenreMS").hide("fast");
+                $("#connectMSGenre").hide("fast");
+                $("#btnConnectMSGenre").hide("fast");
+                $("#connectCatMS").hide("fast");
+                $("#connectMSCat").hide("fast");
+                $("#btnConnectMSCat").hide("fast");
+                $("#connectProducerMS").hide("fast");
+                $("#connectMSProducer").hide("fast");
+                $("#btnConnectMSProducer").hide("fast");
+                $("#connectDirectorMS").fadeIn("slow");
+                $("#connectMSDirector").fadeIn("slow");
+                $("#btnConnectMSDirector").fadeIn("slow");
+
+                $("#connectdirectorms").change(function(){
+                    let form1 = $(this).val()
+                    console.log(form1)
+                    $("#connectmsdirector").change(function(){
+                        let form2 = $(this).val()
+                        console.log(form2)
+                        if((form1 && form2) != 0){
+                            $("#btnConnectMSDirector button").removeAttr("disabled")
+                        }
+                    })
+                })
+            }
+
+            if(forma == 5){
+                $("#connectActorMS").hide("fast");
+                $("#connectMSActor").hide("fast");
+                $("#btnConnectActorMS").hide("fast");
+                $("#connectGenreMS").hide("fast");
+                $("#connectMSGenre").hide("fast");
+                $("#btnConnectMSGenre").hide("fast");
+                $("#connectCatMS").hide("fast");
+                $("#connectMSCat").hide("fast");
+                $("#btnConnectMSCat").hide("fast");
+                $("#connectDirectorMS").hide("slow");
+                $("#connectMSDirector").hide("slow");
+                $("#btnConnectMSDirector").hide("slow");
+                $("#connectProducerMS").fadeIn("slow");
+                $("#connectMSProducer").fadeIn("slow");
+                $("#btnConnectMSProducer").fadeIn("slow");
+
+                $("#connectproducerms").change(function(){
+                    let form1 = $(this).val()
+                    console.log(form1)
+                    $("#connectmsproducer").change(function(){
+                        let form2 = $(this).val()
+                        console.log(form2)
+                        if((form1 && form2) != 0){
+                            $("#btnConnectMSProducer button").removeAttr("disabled")
+                        }
+                    })
+                })
+            }
+        }) 
+    }    
 }
